77095d819d6617389dd30dc2d9dfbd732a7245ce
prasadp <prasad.patel12404@gmail.com>
2025-05-05 08:11:10 +0530

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5f6FFF",
+      },
+    },
   },
   plugins: [],
 };
